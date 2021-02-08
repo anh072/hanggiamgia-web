51ce3104c74a8ca3c74d826c4d6c85c61917255e
Anh Nguyen <anh.nguyen221193@gmail.com>
2021-02-08 11:33:05 +1100

    changed apiBaseUrl

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,7 @@ const config = {
   auth0Domain: "dev-d5keivxi.au.auth0.com",
   auth0ClientId: "aKW5BK1mm4viJ1m4Tlbg89FAsSJ8sL3y",
   auth0ApiAudience: 'https://giare.vn/api/v1',
-  apiBaseUrl: 'http://localhost:5000/api/v1',
+  apiBaseUrl: 'https://6tw1z2cth4.execute-api.ap-southeast-2.amazonaws.com',
   claimNamespace: 'https://giare.vn/',
   localTimezone: 'Asia/Ho_Chi_Minh'
 };
